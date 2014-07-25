a1c0e6f72618b4f8c01ffb4e2f55ea16b51a8455
fuqinho <naoki.fukino@gmail.com>
2014-07-25 09:03:56 -0700

    Definition for GHC instruction.

---
+++ simulator/ghc_instruction.h
@@ -0,0 +1,48 @@
+#include <vector>
+
+enum GHCMnemonic {
+  MOV,
+  INC,
+  DEC,
+  ADD,
+  SUB,
+  MUL,
+  DIV,
+  AND,
+  OR,
+  XOR,
+  JLT,
+  JEQ,
+  JGT,
+  INT,
+  HLT
+};
+
+enum GHCRegister {
+  A = 0,
+  B,
+  C,
+  D,
+  E,
+  F,
+  G,
+  H,
+  PC
+};
+
+enum GHCArgumentType {
+  Constant,
+  Register,
+  Memory
+};
+
+struct GHCArgument {
+  GHCArgumentType type;
+  unsigned int id;
+};
+
+struct GHCInstruction {
+  GHCMnemonic mnemonic;
+  std::vector<GHCArgument> arguments;
+};
+
